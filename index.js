1b5d27adf2a05a103b66de80d2f96ad1890c62e5
Ivan <ivan@Ivans-MacBook-Pro.local>
2018-12-20 15:02:59 -0500

    Title: Radio / Input / Checkbos * added functionality to select a radio and  checbox * Added functionality to select an id for input writting

--- index.js
+++ index.js
@@ -13,16 +13,27 @@ async function startScrape(){
     await page.click('#password');
     await page.keyboard.type('Haze!20!');
     await page.click('#enter');
-    await page.waitFor('#test');
-    await page.click('#test');
+    await page.waitFor('#modules');
+    await page.click('#modules');
     await page.waitFor('body > app > main > group-rfis > div > div > div > div.ng-star-inserted > p-datatable > div > div.ui-datatable-scrollable-wrapper.ui-helper-clearfix.max-height.ng-star-inserted > div > div.ui-datatable-scrollable-body > div > table > tbody > tr:nth-child(2)');
     await page.click('body > app > main > group-rfis > div > div > div > div.ng-star-inserted > p-datatable > div > div.ui-datatable-scrollable-wrapper.ui-helper-clearfix.max-height.ng-star-inserted > div > div.ui-datatable-scrollable-body > div > table > tbody > tr:nth-child(2)');
-    await page.waitFor('body > app > main > group-rfis > div > div > div > div.ng-star-inserted > p-datatable > div > div.ui-datatable-scrollable-wrapper.ui-helper-clearfix.max-height.ng-star-inserted > div > div.ui-datatable-scrollable-body > div > table > tbody > tr:nth-child(3) > td:nth-child(2) > span > div')
-    await page.click('body > app > main > group-rfis > div > div > div > div.ng-star-inserted > p-datatable > div > div.ui-datatable-scrollable-wrapper.ui-helper-clearfix.max-height.ng-star-inserted > div > div.ui-datatable-scrollable-body > div > table > tbody > tr:nth-child(3)')
+    await page.waitFor('body > app > main > group-rfis > div > div > div > div.ng-star-inserted > p-datatable > div > div.ui-datatable-scrollable-wrapper.ui-helper-clearfix.max-height.ng-star-inserted > div > div.ui-datatable-scrollable-body > div > table > tbody > tr:nth-child(3) > td:nth-child(2)')
+    await page.click('body > app > main > group-rfis > div > div > div > div.ng-star-inserted > p-datatable > div > div.ui-datatable-scrollable-wrapper.ui-helper-clearfix.max-height.ng-star-inserted > div > div.ui-datatable-scrollable-body > div > table > tbody > tr:nth-child(3) > td:nth-child(2)')
     await page.waitFor("#cdk-describedby-message-container");
     await page.click('body > app > main > rfi-answers-detail > div > div.rfi-right-side > div > div > button:nth-child(2) > div.mat-button-ripple.mat-ripple');
     await page.waitFor(10000)
-    await page.click('#questionId_3568234163');
+    await page.waitForSelector('#mat-radio-135');
+    await page.click('#mat-radio-135 > label > div')
+    await page.waitFor(2000);
+    await page.click('#mat-checkbox-3 > label > div');
+    await page.waitFor(2000);
+    await page.click("[id='213190_findings']");
+    await page.keyboard.type('Best Farms Ever');
+
+    // await page.focus('#mat-radio-135')
+    // await page.click('mat-radio-button#mat-radio-135');
+    // await page.click('#mat-radio-138-input');
+
 };
 
-startScrape()
+startScrape();
