3141e30808d48e161a5d19109b0427ccdb419ef4
Ivan <ivan@Ivans-MacBook-Pro.local>
2019-01-02 18:07:36 -0500

    Title: File upload * Added file upload functionality

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const pupp = require('puppeteer');
+const testFile = require('../hellowithwaves.png')
+console.log(testFile)
 require('dotenv').config();
 
 async function startScrape(){
@@ -28,22 +30,36 @@ async function startScrape(){
     await page.waitFor(2000);
     await page.click("#mat-option-99 > span.mat-option-text");
     await page.waitFor(2000);
-    await page.click("[id='213190_findings']");
-    await page.keyboard.type('Best Farms Ever');
+    // await page.click("[id='213190_findings']");
+    // await page.keyboard.type('Best Farms Ever');
     await page.click("#mat-radio-2 > label > div ");
     await page.waitFor(2000);
-    await page.click('#mat-dialog-0 > rfi-confirm > div > div:nth-child(3) > button.settings-content-btn.blue-btn.mat-raised-button.ng-star-inserted')
+    // await page.click('#mat-dialog-0 > rfi-confirm > div > div:nth-child(3) > button.settings-content-btn.blue-btn.mat-raised-button.ng-star-inserted')
+    // await page.waitFor(2000);
+    // await page.click('#mat-slide-toggle-1 > label > div');
+    // await page.waitFor(1000);
+    // await page.click('#mat-radio-11 > label > div');
+    // await page.waitFor(1000);
+    // await page.click('[id="213280"]');
+    // await page.waitFor(500)
+    // await page.keyboard.type('34');
+    // await page.click('[id="213280_findings"]');
+    // await page.keyboard.type('We found the number of employees')
+    await page.click("#mat-radio-29 > label > div");
+    await page.click("#mat-radio-32 > label > div");
+    await page.click("#mat-radio-35 > label > div");
+    await page.click("#mat-radio-38 > label > div");
+    await page.click('#questionId_global_2_1 > div button');
     await page.waitFor(2000);
-    await page.click('#mat-slide-toggle-1 > label > div');
-    await page.waitFor(1000);
-    await page.click('#mat-radio-11 > label > div');
-    await page.waitFor(1000);
-    await page.click('[id="213280"]');
-    await page.waitFor(500)
-    await page.keyboard.type('34');
-    await page.click('[id="213280_findings"]');
-    await page.keyboard.type('We found the number of employees')
+    // await page.click('#__filestack-picker > div > div.fsp-picker.fsp-picker--active > div.fsp-modal > div.fsp-modal__body > div.fsp-content > div > div > div');
+    const input = await page.$('#fsp-fileUpload');
+    await input.uploadFile(testFile)
     await page.waitFor(2000);
+    await page.click('#__filestack-picker > div > div > div.fsp-modal > div.fsp-modal__body > div.fsp-content.fsp-content--selected-items > div > div.fsp-summary__body > div.fsp-footer.fsp-footer--appeared > div > span.fsp-footer__nav--right');
+    await page.waitFor(10000);
+    
+
+
     await browser.close();
 };
 
