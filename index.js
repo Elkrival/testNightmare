9c07cec2eb0679e07fe4cd313fb04801487d306f
Ivan <ivan@Ivans-MacBook-Pro.local>
2019-01-03 16:57:30 -0500

    Title: VF * Workflow to complete a survey from beginning to end * Added env variables for id and files to .env * Changed .env variables on code * Verified browser walkthrough

--- index.js
+++ index.js
@@ -16,48 +16,134 @@ async function startScrape(){
     await page.click('#enter');
     await page.waitFor('#modules');
     await page.click('#modules');
-    await page.waitFor('[id="5c2d1fdfb75bee74b6154421"]')
-    await page.click('[id="5c2d1fdfb75bee74b6154421"]')
-    await page.waitFor("[id='5c06c1c8d92248363acf47cf']")
-    await page.click('[id="5c06c1c8d92248363acf47cf"]')
+    await page.waitFor(`[id="${process.env.SURVEY_TYPE_ID}"]`)
+    await page.click(`[id="${process.env.SURVEY_TYPE_ID}"]`)
+    await page.waitFor(`[id="${process.env.SURVEY_ID}"]`)
+    await page.click(`[id="${process.env.SURVEY_ID}"]`)
     await page.waitFor("#cdk-describedby-message-container");
     await page.click('#reset_answers')
-    await page.waitFor(10000)
+    await page.waitFor(7000)
     await page.waitForSelector('#facility_country');
     await page.click('#facility_country')
     await page.waitFor(2000);
     await page.click("#mat-option-99 > span.mat-option-text");
     await page.waitFor(2000);
-    // await page.click("[id='213190_findings']");
-    // await page.keyboard.type('Best Farms Ever');
     await page.click("#mat-radio-2 > label > div ");
     await page.waitFor(2000);
-    // await page.click('#mat-dialog-0 > rfi-confirm > div > div:nth-child(3) > button.settings-content-btn.blue-btn.mat-raised-button.ng-star-inserted')
-    // await page.waitFor(2000);
-    // await page.click('#mat-slide-toggle-1 > label > div');
-    // await page.waitFor(1000);
-    // await page.click('#mat-radio-11 > label > div');
-    // await page.waitFor(1000);
-    // await page.click('[id="213280"]');
-    // await page.waitFor(500)
-    // await page.keyboard.type('34');
-    // await page.click('[id="213280_findings"]');
-    // await page.keyboard.type('We found the number of employees')
+    await page.click("#mat-radio-27 > label > div");
+    await page.waitFor(1000);
     await page.click("#mat-radio-29 > label > div");
+    await page.waitFor(1000);
     await page.click("#mat-radio-32 > label > div");
+    await page.waitFor(1000);
     await page.click("#mat-radio-35 > label > div");
+    await page.waitFor(1000);
     await page.click("#mat-radio-38 > label > div");
+    await page.waitFor(1000);
     await page.click('#questionId_global_2_1 > div button');
     await page.waitFor(2000);
-    // await page.click('#__filestack-picker > div > div.fsp-picker.fsp-picker--active > div.fsp-modal > div.fsp-modal__body > div.fsp-content > div > div > div');
-    const input = await page.$('#fsp-fileUpload');
-    await input.uploadFile('../hellowithwaves.png')
+    let input = await page.$('#fsp-fileUpload');
+    await input.uploadFile(process.env.FILE)
     await page.waitFor(2000);
     await page.click('#__filestack-picker > div > div > div.fsp-modal > div.fsp-modal__body > div.fsp-content.fsp-content--selected-items > div > div.fsp-summary__body > div.fsp-footer.fsp-footer--appeared > div > span.fsp-footer__nav--right');
+    await page.waitFor(2000);
+    let global3 = await page.evaluate(() => document.querySelector('#global_3').children[0].id);// This is a test for radio selectors on isolating our 
+    await page.waitFor(1000);
+    await page.click('#' + global3 + '> label > div');
+    await page.waitFor(1000);
+    await page.click("#mat-radio-41 > label > div");
+    await page.waitFor(500);
+    await page.click("#mat-radio-44 > label > div");
+    await page.waitFor(500);
+    await page.click("#mat-radio-47 > label > div");
+    await page.waitFor(500);
+    await page.click("#mat-radio-50 > label > div");
+    await page.waitFor(500);
+    await page.click('#questionId_global_3_1 > div button');
+    await page.waitFor('#fsp-fileUpload');
+    input = await page.$('#fsp-fileUpload');
+    await input.uploadFile(process.env.FILE)
+    await page.waitFor(2000);
+    await page.click('#__filestack-picker > div > div > div.fsp-modal > div.fsp-modal__body > div.fsp-content.fsp-content--selected-items > div > div.fsp-summary__body > div.fsp-footer.fsp-footer--appeared > div > span.fsp-footer__nav--right');
+    await page.waitFor(1000);
+    await page.click('#mat-radio-15 > label > div');
+    await page.waitFor(1000);
+    await page.click('#mat-radio-8 > label > div');
+    await page.waitFor(1000);
+    await page.click('#mat-radio-11 > label > div');
+    await page.waitFor(1000);
+    await page.click('#mat-slide-toggle-1 > label');
+    await page.waitFor(1000);
+    await page.click('#mat-radio-17 > label > div');
+    await page.waitFor(1000);
+    await page.click('#mat-radio-53 > label > div');
+    await page.waitFor(1000);
+    await page.click('#mat-radio-57 > label > div');
+    await page.waitFor(2000);
+    await page.click('#mat-radio-59 > label > div');
+    await page.waitFor(1000);
+    await page.click('#mat-radio-62 > label > div');
+    await page.waitFor(1000);
+    await page.click('#mat-radio-65 > label > div');
+    await page.waitFor(1000);
+    await page.click('#mat-radio-68 > label > div');
+    await page.waitFor(1000);
+    await page.click('#mat-radio-71 > label > div');
+    await page.waitFor(1000);
+    await page.click('#mat-radio-74 > label > div');
+    await page.waitFor(1000);
+    await page.click('#mat-radio-77 > label > div');
+    await page.waitFor(1000);
+    await page.click('#questionId_foot_7_a > div button');
+    await page.waitFor(1000);
+    input = await page.$('#fsp-fileUpload');
+    await page.waitFor(1000);
+    await input.uploadFile(process.env.FILE)
+    await page.waitFor(1000)
+    await page.click('#__filestack-picker > div > div > div.fsp-modal > div.fsp-modal__body > div.fsp-content.fsp-content--selected-items > div > div.fsp-summary__body > div.fsp-footer.fsp-footer--appeared > div > span.fsp-footer__nav--right');
+    await page.waitFor(3000);
+    await page.click('#mat-slide-toggle-2 > label');
+    await page.waitFor(1000);
+    await page.click('#mat-radio-20 > label > div');
+    await page.waitFor(1000);
+    await page.click('#fin_1')
+    await page.waitFor(2000);
+    await page.click("#mat-option-256 > span.mat-option-text");
+    await page.waitFor(1000);
+    await page.click('#fin_2')
+    await page.waitFor(2000);
+    await page.click("#mat-option-266 > span.mat-option-text");
+    await page.waitFor(1000)
+    await page.click('#mat-checkbox-2 > label > div');
+    await page.waitFor(1000);
+    await page.click('#mat-checkbox-1 > label > div');
+    await page.waitFor(1000);
+    await page.click("[id='fin_4']");
+    await page.keyboard.type('5');
+    await page.waitFor(1000);
+    await page.click("[id='fin_5']");
+    await page.keyboard.type('5');
+    await page.waitFor(1000);
+    await page.click('#mat-radio-81 > label > div');
+    await page.waitFor(1000);
+    await page.click('#mat-radio-92 > label > div');
+    await page.waitFor(1000);
+    await page.click('#mat-radio-83 > label > div');
+    await page.waitFor(1000);
+    await page.click('#mat-radio-86 > label > div');
+    await page.waitFor(1000);
+    await page.click('#mat-radio-89 > label > div');
+    await page.waitFor(3000);
+    await page.click('#mat-slide-toggle-3 > label');
+    await page.waitFor(1000);
+    await page.click('#mat-radio-24 > label > div');
+    await page.waitFor(1000);
+    await page.click('#mat-slide-toggle-4 > label');
+    await page.waitFor(1000);
+    await page.click('#mat-radio-27 > label > div');
+    await page.waitFor(1000);
+    await page.click('#mat-slide-toggle-5 > label');
     await page.waitFor(10000);
-    
-
-
     await browser.close();
 };
 
