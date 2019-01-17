da56fbd23b9981d2934b37a48ecb2e8c43fb8246
Ivan <ivan@Ivans-MacBook-Pro.local>
2019-01-17 18:18:48 -0500

    Title: Streams * Created Streams of values from intercepting the XHR request * Wrote item to file * imported file to tests * Iterated through values to test

--- index.js
+++ index.js
@@ -1,52 +1,57 @@
 const pupp = require('puppeteer');
 const parentChildReveal = require('./response_helpers');
 const dynamicQuestionFilter = require('./question_filters');
-console.log(dynamicQuestionFilter, '&&&&&&&&&&&&&&&&&&&&&&&&&&0000000000000000000000')
+const fs = require('fs');
 require('dotenv').config();
 const events = [
     'dialog',
     'request',
     'response'
 ]
-
-async function startScrape(){
-    let array=[]
-    const browser = await pupp.launch({ headless: false, devtools: false });
+let hidden;
+let radio;
+let questionArray=[]
+let file;
+let dropdowns;
+let number;
+let parentChildRevealArray;
+let browser
+async function initScrape(){
+    browser = await pupp.launch({ headless: true });
     const page = await browser.newPage()
-    await page.setViewport({ width: 900, height:  1080 })
+    await page.setViewport({ width: 1280, height: 1307 })
     events.forEach(eventType => {
         page.on(eventType, async (eventFilterFunc)=>{
         if(eventType === 'dialog') {
             return eventFilterFunc.accept()
         }
         else if(eventType === 'response') {
-            console.log(eventFilterFunc.url())
             if(eventFilterFunc.url().includes('rfis/answers/') && !eventFilterFunc.url().includes('rfis/answers/templates')) {
                 let rfiResponse = await eventFilterFunc.json().then(res => res).catch(e => console.error(e.message))
                 if(rfiResponse.hasOwnProperty('pages') && Array.isArray(rfiResponse.pages) && rfiResponse.pages.length > 0){
                     let components = await rfiResponse.pages.map(el => el.components)
                     await components.forEach((el) => {
                         for (const element in el) {
-                            array.push(el[element])
+                            questionArray.push(el[element])
                         }
                     })
-                    array.sort((a,b) => a.position - b.position);  
-                    let hidden = await dynamicQuestionFilter(array, 'hidden');
-                    console.log(hidden)
-                    let radio = await dynamicQuestionFilter(array,'radio');
+                    questionArray.sort((a,b) => a.position - b.position);  
+                    hidden = await dynamicQuestionFilter(questionArray, 'hidden');
                     
-                    console.log(radio)
-                    let file = await dynamicQuestionFilter(array, 'file');
+                    fs.writeFile('test.json',JSON.stringify(hidden, null, 2), 'utf-8', function(err){
+                        if(err) {
+                            console.error(err.message, 'there was an error')
+                        }
+                        else{
+                            console.log('wrote data on a file')
+                        }
+                    })
+                    radio = await dynamicQuestionFilter(questionArray,'radio');
+                    file = await dynamicQuestionFilter(questionArray, 'file');
+                    dropdowns = await dynamicQuestionFilter(questionArray, 'dropdown');
+                    number = await dynamicQuestionFilter(questionArray, 'text');
                     
-                    console.log(file)
-                    let dropdowns = await dynamicQuestionFilter(array, 'dropdown');
-                    
-                    console.log(dropdowns)
-                    let number = await dynamicQuestionFilter(array, 'text');
-                    
-                    console.log(number)
-                    let parentChildRevealArray = await parentChildReveal(array, hidden);
-                    console.log(parentChildRevealArray)
+                    parentChildRevealArray = await parentChildReveal(questionArray, hidden);
                 }
                 else {
                     console.log('ಠ_ಠ ¯\_(ツ)_/¯ Nothing To See here ¯\_(ツ)_/¯ಠ_ಠ')
@@ -63,136 +68,137 @@ async function startScrape(){
     await page.click('#enter');
     await page.waitFor('#modules');
     await page.click('#modules');
-    await page.waitFor(`[id="${process.env.SURVEY_TYPE_ID}"]`)
+    // await page.waitFor(`[id="${process.env.SURVEY_TYPE_ID}"]`)
+    await page.waitFor(1000);
     await page.click(`[id="${process.env.SURVEY_TYPE_ID}"]`)
-    await page.waitFor(`[id="${process.env.SURVEY_ID}"]`)
+    // await page.waitFor(`[id="${process.env.SURVEY_ID}"]`)
+    await page.waitFor(1000);
     await page.click(`[id="${process.env.SURVEY_ID}"]`)
     await page.waitFor("#cdk-describedby-message-container");
     await page.click('#reset_answers')
-    array = []
-    await page.waitFor(7000)
-    await page.waitForSelector('#facility_country');
-    await page.click('#facility_country')
-    await page.waitFor(2000);
-    await page.click("#mat-option-99 > span.mat-option-text");
-    await page.waitFor(2000);
-    await page.click("#mat-radio-2 > label > div ");
-    await page.waitFor(2000);
-    await page.click("#mat-radio-27 > label > div");
-    await page.waitFor(1000);
-    await page.click("#mat-radio-29 > label > div");
-    await page.waitFor(1000);
-    await page.click("#mat-radio-32 > label > div");
-    await page.waitFor(1000);
-    await page.click("#mat-radio-35 > label > div");
-    await page.waitFor(1000);
-    await page.click("#mat-radio-38 > label > div");
-    await page.waitFor(1000);
-    await page.click('#questionId_global_2_1 > div button');
-    await page.waitFor(2000);
-    let input = await page.$('#fsp-fileUpload');
-    await input.uploadFile(process.env.FILE)
-    await page.waitFor(2000);
-    await page.click('#__filestack-picker > div > div > div.fsp-modal > div.fsp-modal__body > div.fsp-content.fsp-content--selected-items > div > div.fsp-summary__body > div.fsp-footer.fsp-footer--appeared > div > span.fsp-footer__nav--right');
-    await page.waitFor(2000);
-    let global3 = await page.evaluate(() => document.querySelector('#global_3').children[0].id);// This is a test for radio selectors on isolating our 
-    await page.waitFor(1000);
-    await page.click('#' + global3 + '> label > div');
-    await page.waitFor(1000);
-    await page.click("#mat-radio-41 > label > div");
-    await page.waitFor(500);
-    await page.click("#mat-radio-44 > label > div");
-    await page.waitFor(500);
-    await page.click("#mat-radio-47 > label > div");
-    await page.waitFor(500);
-    await page.click("#mat-radio-50 > label > div");
-    await page.waitFor(500);
-    await page.click('#questionId_global_3_1 > div button');
-    await page.waitFor('#fsp-fileUpload');
-    input = await page.$('#fsp-fileUpload');
-    await input.uploadFile(process.env.FILE)
-    await page.waitFor(2000);
-    await page.click('#__filestack-picker > div > div > div.fsp-modal > div.fsp-modal__body > div.fsp-content.fsp-content--selected-items > div > div.fsp-summary__body > div.fsp-footer.fsp-footer--appeared > div > span.fsp-footer__nav--right');
-    await page.waitFor(1000);
-    await page.click('#mat-radio-15 > label > div');
-    await page.waitFor(1000);
-    await page.click('#mat-radio-8 > label > div');
-    await page.waitFor(1000);
-    await page.click('#mat-radio-11 > label > div');
-    await page.waitFor(1000);
-    await page.click('#mat-slide-toggle-1 > label');
-    await page.waitFor(1000);
-    await page.click('#mat-radio-17 > label > div');
-    await page.waitFor(1000);
-    await page.click('#mat-radio-53 > label > div');
-    await page.waitFor(1000);
-    await page.click('#mat-radio-57 > label > div');
-    await page.waitFor(2000);
-    await page.click('#mat-radio-59 > label > div');
-    await page.waitFor(1000);
-    await page.click('#mat-radio-62 > label > div');
-    await page.waitFor(1000);
-    await page.click('#mat-radio-65 > label > div');
-    await page.waitFor(1000);
-    await page.click('#mat-radio-68 > label > div');
-    await page.waitFor(1000);
-    await page.click('#mat-radio-71 > label > div');
-    await page.waitFor(1000);
-    await page.click('#mat-radio-74 > label > div');
-    await page.waitFor(1000);
-    await page.click('#mat-radio-77 > label > div');
-    await page.waitFor(1000);
-    await page.click('#questionId_foot_7_a > div button');
-    await page.waitFor(1000);
-    input = await page.$('#fsp-fileUpload');
-    await page.waitFor(1000);
-    await input.uploadFile(process.env.FILE)
-    await page.waitFor(1000)
-    await page.click('#__filestack-picker > div > div > div.fsp-modal > div.fsp-modal__body > div.fsp-content.fsp-content--selected-items > div > div.fsp-summary__body > div.fsp-footer.fsp-footer--appeared > div > span.fsp-footer__nav--right');
-    await page.waitFor(3000);
-    await page.click('#mat-slide-toggle-2 > label');
-    await page.waitFor(1000);
-    await page.click('#mat-radio-20 > label > div');
-    await page.waitFor(1000);
-    await page.click('#fin_1')
-    await page.waitFor(2000);
-    await page.click("#mat-option-256 > span.mat-option-text");
-    await page.waitFor(1000);
-    await page.click('#fin_2')
-    await page.waitFor(2000);
-    await page.click("#mat-option-266 > span.mat-option-text");
-    await page.waitFor(1000)
-    await page.click('#mat-checkbox-2 > label > div');
-    await page.waitFor(1000);
-    await page.click('#mat-checkbox-1 > label > div');
-    await page.waitFor(1000);
-    await page.click("[id='fin_4']");
-    await page.keyboard.type('5');
-    await page.waitFor(1000);
-    await page.click("[id='fin_5']");
-    await page.keyboard.type('5');
-    await page.waitFor(1000);
-    await page.click('#mat-radio-81 > label > div');
-    await page.waitFor(1000);
-    await page.click('#mat-radio-92 > label > div');
-    await page.waitFor(1000);
-    await page.click('#mat-radio-83 > label > div');
-    await page.waitFor(1000);
-    await page.click('#mat-radio-86 > label > div');
-    await page.waitFor(1000);
-    await page.click('#mat-radio-89 > label > div');
-    await page.waitFor(3000);
-    await page.click('#mat-slide-toggle-3 > label');
-    await page.waitFor(1000);
-    await page.click('#mat-radio-24 > label > div');
-    await page.waitFor(1000);
-    await page.click('#mat-slide-toggle-4 > label');
-    await page.waitFor(1000);
-    await page.click('#mat-radio-27 > label > div');
-    await page.waitFor(1000);
-    await page.click('#mat-slide-toggle-5 > label');
-    await page.waitFor(10000);
-    await browser.close();
+    questionArray = []
+    // await page.waitFor(7000)
+    // await page.waitForSelector('#facility_country');
+    // await page.click('#facility_country')
+    // await page.waitFor(2000);
+    // await page.click("#mat-option-99 > span.mat-option-text");
+    // await page.waitFor(2000);
+    // await page.click("#mat-radio-2 > label > div ");
+    // await page.waitFor(2000);
+    // await page.click("#mat-radio-27 > label > div");
+    // await page.waitFor(1000);
+    // await page.click("#mat-radio-29 > label > div");
+    // await page.waitFor(1000);
+    // await page.click("#mat-radio-32 > label > div");
+    // await page.waitFor(1000);
+    // await page.click("#mat-radio-35 > label > div");
+    // await page.waitFor(1000);
+    // await page.click("#mat-radio-38 > label > div");
+    // await page.waitFor(1000);
+    // await page.click('#questionId_global_2_1 > div button');
+    // await page.waitFor(2000);
+    // let input = await page.$('#fsp-fileUpload');
+    // await input.uploadFile(process.env.FILE)
+    // await page.waitFor(2000);
+    // await page.click('#__filestack-picker > div > div > div.fsp-modal > div.fsp-modal__body > div.fsp-content.fsp-content--selected-items > div > div.fsp-summary__body > div.fsp-footer.fsp-footer--appeared > div > span.fsp-footer__nav--right');
+    // await page.waitFor(2000);
+    // let global3 = await page.evaluate(() => document.querySelector('#global_3').children[0].id);// This is a test for radio selectors on isolating our 
+    // await page.waitFor(1000);
+    // await page.click('#' + global3 + '> label > div');
+    // await page.waitFor(1000);
+    // await page.click("#mat-radio-41 > label > div");
+    // await page.waitFor(500);
+    // await page.click("#mat-radio-44 > label > div");
+    // await page.waitFor(500);
+    // await page.click("#mat-radio-47 > label > div");
+    // await page.waitFor(500);
+    // await page.click("#mat-radio-50 > label > div");
+    // await page.waitFor(500);
+    // await page.click('#questionId_global_3_1 > div button');
+    // await page.waitFor('#fsp-fileUpload');
+    // input = await page.$('#fsp-fileUpload');
+    // await input.uploadFile(process.env.FILE)
+    // await page.waitFor(2000);
+    // await page.click('#__filestack-picker > div > div > div.fsp-modal > div.fsp-modal__body > div.fsp-content.fsp-content--selected-items > div > div.fsp-summary__body > div.fsp-footer.fsp-footer--appeared > div > span.fsp-footer__nav--right');
+    // await page.waitFor(1000);
+    // await page.click('#mat-radio-15 > label > div');
+    // await page.waitFor(1000);
+    // await page.click('#mat-radio-8 > label > div');
+    // await page.waitFor(1000);
+    // await page.click('#mat-radio-11 > label > div');
+    // await page.waitFor(1000);
+    // await page.click('#mat-slide-toggle-1 > label');
+    // await page.waitFor(1000);
+    // await page.click('#mat-radio-17 > label > div');
+    // await page.waitFor(1000);
+    // await page.click('#mat-radio-53 > label > div');
+    // await page.waitFor(1000);
+    // await page.click('#mat-radio-57 > label > div');
+    // await page.waitFor(2000);
+    // await page.click('#mat-radio-59 > label > div');
+    // await page.waitFor(1000);
+    // await page.click('#mat-radio-62 > label > div');
+    // await page.waitFor(1000);
+    // await page.click('#mat-radio-65 > label > div');
+    // await page.waitFor(1000);
+    // await page.click('#mat-radio-68 > label > div');
+    // await page.waitFor(1000);
+    // await page.click('#mat-radio-71 > label > div');
+    // await page.waitFor(1000);
+    // await page.click('#mat-radio-74 > label > div');
+    // await page.waitFor(1000);
+    // await page.click('#mat-radio-77 > label > div');
+    // await page.waitFor(1000);
+    // await page.click('#questionId_foot_7_a > div button');
+    // await page.waitFor(1000);
+    // input = await page.$('#fsp-fileUpload');
+    // await page.waitFor(1000);
+    // await input.uploadFile(process.env.FILE)
+    // await page.waitFor(1000)
+    // await page.click('#__filestack-picker > div > div > div.fsp-modal > div.fsp-modal__body > div.fsp-content.fsp-content--selected-items > div > div.fsp-summary__body > div.fsp-footer.fsp-footer--appeared > div > span.fsp-footer__nav--right');
+    // await page.waitFor(3000);
+    // await page.click('#mat-slide-toggle-2 > label');
+    // await page.waitFor(1000);
+    // await page.click('#mat-radio-20 > label > div');
+    // await page.waitFor(1000);
+    // await page.click('#fin_1')
+    // await page.waitFor(2000);
+    // await page.click("#mat-option-256 > span.mat-option-text");
+    // await page.waitFor(1000);
+    // await page.click('#fin_2')
+    // await page.waitFor(2000);
+    // await page.click("#mat-option-266 > span.mat-option-text");
+    // await page.waitFor(1000)
+    // await page.click('#mat-checkbox-2 > label > div');
+    // await page.waitFor(1000);
+    // await page.click('#mat-checkbox-1 > label > div');
+    // await page.waitFor(1000);
+    // await page.click("[id='fin_4']");
+    // await page.keyboard.type('5');
+    // await page.waitFor(1000);
+    // await page.click("[id='fin_5']");
+    // await page.keyboard.type('5');
+    // await page.waitFor(1000);
+    // await page.click('#mat-radio-81 > label > div');
+    // await page.waitFor(1000);
+    // await page.click('#mat-radio-92 > label > div');
+    // await page.waitFor(1000);
+    // await page.click('#mat-radio-83 > label > div');
+    // await page.waitFor(1000);
+    // await page.click('#mat-radio-86 > label > div');
+    // await page.waitFor(1000);
+    // await page.click('#mat-radio-89 > label > div');
+    // await page.waitFor(3000);
+    // await page.click('#mat-slide-toggle-3 > label');
+    // await page.waitFor(1000);
+    // await page.click('#mat-radio-24 > label > div');
+    // await page.waitFor(1000);
+    // await page.click('#mat-slide-toggle-4 > label');
+    // await page.waitFor(1000);
+    // await page.click('#mat-radio-27 > label > div');
+    // await page.waitFor(1000);
+    // await page.click('#mat-slide-toggle-5 > label');
+    // await page.waitFor(10000);
+    await browser.close()
 };
-
-startScrape();
+initScrape()
--- workflow.test.js
+++ workflow.test.js
@@ -1,105 +1,55 @@
 const pupp = require('puppeteer');
-let chai = require('chai');
-let should = chai.should()
-let expect = chai.expect
-const parentChildReveal = require('./response_helpers');
-const dynamicQuestionFilter = require('./question_filters');
+const arrayData = require('./test.json')
+
 require('dotenv').config();
 
     let browser
-    let page
-    let hidden = []
-    let rfiResponse
-    let radio
-    let file
-    let dropdowns
-    let number
-    let questionArray = []
-    let parentChildRevealArray
-    let surveyId 
-    let surveyTypeId
-
-describe('Survey Tests',  async()=>{
-    before(async() =>{
-    const events = [
-        'dialog',
-        'request',
-        'response'
-    ]
-     jest.setTimeout(90000)
-     browser = await pupp.launch();
-     page = await browser.newPage()
-     await events.forEach(eventType => {
-        page.on(eventType, async (eventFilterFunc)=>{
-            if(eventType === 'dialog') {
-                return eventFilterFunc.accept()
-            }
-            else if(eventType === 'response') {
-                if(eventFilterFunc.url().includes('/rfis/answers/') && !eventFilterFunc.url().includes('rfis/answers/templates')){   
-                    await eventFilterFunc.json().then(res => rfiResponse = res).catch(e => console.error(e.message))
-
-                    if(rfiResponse.hasOwnProperty('pages') && Array.isArray(rfiResponse.pages) && rfiResponse.pages.length > 0){
-                        let components = await rfiResponse.pages.map(el => el.components)
-                        await components.forEach((el) => {
-                        for (const element in el) {
-                            questionArray.push(el[element])
-                            }
-                        })
-                        questionArray.sort((a,b) => a.position - b.position);  
-                        hidden = await dynamicQuestionFilter(questionArray, 'hidden');
-                        radio = await dynamicQuestionFilter(questionArray,'radio');
-                        file = await dynamicQuestionFilter(questionArray, 'file');
-                        dropdowns = await dynamicQuestionFilter(questionArray, 'dropdown');
-                        number = await dynamicQuestionFilter(questionArray, 'text');
-                        parentChildRevealArray = await parentChildReveal(questionArray, hidden);
-                    }
-                    else {
-                        console.log('ಠ_ಠ ¯\_(ツ)_/¯ Nothing To See here ¯\_(ツ)_/¯ಠ_ಠ')
-                    }
-                }
-            }
+    // let page
+    // let hidden = []
+    // let rfiResponse
+    // let radio
+    // let file
+    // let dropdowns
+    // let number
+    // let questionArray = []
+    // let parentChildRevealArray
+    // let surveyId 
+    // let surveyTypeId
+    describe('Survey Tests', ()=>{
+        jest.setTimeout(100000)
+        beforeAll(async() =>{
+            browser = await pupp.launch({ headless: true });
+            const page = await browser.newPage()
+            page.on('dialog', async(event) =>{
+                return event.accept()
+            })
+            await page.goto('http://localhost:8080/#/auth/login');
+            await page.click('#email');
+            await page.keyboard.type(process.env.USERNAME);
+            await page.click('#password');
+            await page.keyboard.type(process.env.PASSWORD);
+            await page.click('#enter');
+            await page.waitFor('#modules');
+            await page.click('#modules');
+            // await page.waitFor(`[id="${process.env.SURVEY_TYPE_ID}"]`)
+            await page.waitFor(1000);
+            await page.click(`[id="${process.env.SURVEY_TYPE_ID}"]`)
+            // await page.waitFor(`[id="${process.env.SURVEY_ID}"]`)
+            await page.waitFor(1000);
+            await page.click(`[id="${process.env.SURVEY_ID}"]`)
+            await page.waitFor("#cdk-describedby-message-container");
+            await page.click('#reset_answers')
+    })
+    arrayData.forEach(async(el) => {
+        it('will verify if hidden questions are available',async() =>{
+            let result = await page.evaluate((questionId) => {
+                return document.querySelector('#' + questionId)
+            }, el.id)
+            await expect(result).toBeTruthy()
         })
     })
-    await page.goto('http://localhost:8080/#/auth/login');
-    await page.click('#email');
-    await page.keyboard.type(process.env.USERNAME);
-    await page.click('#password');
-    await page.keyboard.type(process.env.PASSWORD);
-    await page.click('#enter');
-    await page.waitFor('#modules');
-    await page.click('#modules');
-    await page.waitFor(`[id="${process.env.SURVEY_TYPE_ID}"]`)
-    await page.click(`[id="${process.env.SURVEY_TYPE_ID}"]`)
-    await page.waitFor(`[id="${process.env.SURVEY_ID}"]`)
-    await page.click(`[id="${process.env.SURVEY_ID}"]`)
-    await page.waitFor("#cdk-describedby-message-container");
-    await page.click('#reset_answers')
-    // await hiddenTests(hidden)
-    await hidden.forEach(async question => await grr(question))
-    })
-    await it('should test true for array and length', async() =>{
-        await expect(questionArray.length).to.be.equal(0)
-    })
-    // hidden = dynamicQuestionFilter(questionArray, 'hidden');
 
-        for (let hiddenQuestion = 0; hiddenQuestion < hidden.length; hiddenQuestion++) {
-             it('should do this',  ()=>{
-                console.log(hidden[hiddenQuestion]);
-            })
-            await grr(question)
-        }
-
-        
-    after(async()=>{
-        await browser.close()
+    afterAll(()=>{
+        browser.close()
     })
-async function grr(question){
-    await describe('hidden questions',async()=>{
-        let testQuestion
-        console.log('inside')
-    await it('should do this', async()=>{
-        console.log('inside two')
-    })
-    })
-}
 })
